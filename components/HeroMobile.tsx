defc5ef7ac5d7a98bbbbd36759b4bd92367edcca
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2026-04-06 14:33:23 +0100

    clean

--- components/HeroMobile.tsx
+++ components/HeroMobile.tsx
@@ -2,7 +2,14 @@ const HeroMobile = () => {
   return (
     <div className="relative">
       <div className="w-screen h-screen top-0 left-0 fixed bg-linear-to-b from-black/20 via-black/50 to-black/90"></div>
-      <video className="object-cover h-screen" autoPlay muted loop>
+      <video
+        className="object-cover h-screen"
+        autoPlay
+        muted
+        loop
+        playsInline
+        controls={false}
+      >
         <source src="/vid.mp4" type="video/mp4" />
       </video>
     </div>
