09aa7e21dd99af86881f1bd6348f1fed54789705
Derin Owoade <85637610+derin-art@users.noreply.github.com>
2026-04-07 11:01:33 +0100

    video fix for iso

--- components/HeroMobile.tsx
+++ components/HeroMobile.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 import { useEffect, useRef } from "react";
+import ReactPlayer from "react-player";
 
 const HeroMobile = () => {
   const videoRef: any = useRef(null);
@@ -22,7 +23,19 @@ const HeroMobile = () => {
       {/* Added pointer-events-none so the overlay doesn't block interactions if needed later */}
       <div className="w-screen h-screen top-0 left-0 fixed bg-linear-to-b from-black/20 via-black/50 to-black/90 pointer-events-none"></div>
 
-      <video
+      <ReactPlayer
+        ref={videoRef}
+        src="/vid.mp4"
+        playing
+        muted
+        loop
+        controls={false}
+        width="100%"
+        style={{ height: "100vh" }}
+        height="100%"
+        className=" top-0 left-0 object-cover"
+      />
+      {/*    <video
         ref={videoRef}
         className="object-cover w-full h-screen"
         autoPlay
@@ -33,7 +46,7 @@ const HeroMobile = () => {
         controls={false}
       >
         <source src="/vid.mp4" type="video/mp4" />
-      </video>
+      </video> */}
     </div>
   );
 };
